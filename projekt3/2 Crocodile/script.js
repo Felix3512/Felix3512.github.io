7fbff552c941e9bb56d8f657815244eed3cbbb31
Felix3512 <felix.heri@realgymnasiet.se>
2026-05-19 11:39:13 +0200

    Dark Mode
    Vid knapp tryck byts hemsidan till ett dark mode och vid ett till knapp tryck byts det tillbaka till "light mode"

--- projekt3/2 Crocodile/script.js
+++ projekt3/2 Crocodile/script.js
@@ -1,4 +1,4 @@
-console.log ("dfdfdf")
+// console.log ("page loaded")
 
 const hamburger = document.getElementById("hamburger");
 const navLinks = document.getElementById("nav-links");
@@ -10,6 +10,48 @@ hamburger.addEventListener("click", function() {
 
 const colors = ["#f9c74f", "#90be6d", "#43aa8b", "#577590", "#f94144", "#f3722c"];
 
+var allText = document.querySelectorAll("p, .nav-title, nav a")
+var nav = document.querySelector("nav");
+var main = document.querySelector("main");
+var hero = document.querySelector(".hero");
+var footer = document.querySelector("footer");
+
+var currentMode = "light"
+
 function changeColor() {
-  const random = Math.floor(Math.random() * colors.length);
-  document.body.style.backgroundColor = colors[random];}
+
+  // const random = Math.floor(Math.random() * colors.length);
+  // document.body.style.backgroundColor = colors[random];
+  // console.log("button pressed")
+  
+  if(currentMode == "light") {
+
+    allText.forEach((text) => {
+      text.style.color = "var(--text-dark)"
+    })
+
+    nav.style.backgroundColor = "var(--bg-nav-dark)"
+    main.style.backgroundColor = "var(--bg-main-dark)"
+    footer.style.backgroundColor = "var(--bg-footer-dark)"
+    hero.style.backgroundColor = "var(--bg-hero-dark)"
+    
+    currentMode = "dark"
+    console.log("dark activated")
+
+  }else {
+
+    allText.forEach((text) => {
+      text.style.color = "var(--text-light)"
+    })
+
+    nav.style.backgroundColor = "var(--bg-nav-light)"
+    main.style.backgroundColor = "var(--bg-main-light)"
+    footer.style.backgroundColor = "var(--bg-footer-light)"
+    hero.style.backgroundColor = "var(--bg-nav-light)"
+    
+    currentMode = "light"
+
+    
+  }
+
+}
